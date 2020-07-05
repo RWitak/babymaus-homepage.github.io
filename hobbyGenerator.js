fb9e06f61906ed2b2d444bb6284f1ba2ef21f81d
RWitak <rafael.witak@gmail.com>
2020-07-05 13:53:38 +0200

    add new random hobbies & date specific hobby logic

--- hobbyGenerator.js
+++ hobbyGenerator.js
@@ -14,10 +14,23 @@ var hobbies = [
         'lieb Schauen',
         'immer Kriegen, was sie will, ',
         'immer genau Wissen, was sie grad will, ',
-        'Kuscheln'
+        'Kuscheln',
+        'sich auf ihren Geburtstag Freuen',
+        'im Blumentopf Sitzen',
+        'fast eine ganze Babymausportion Essen',
+        'fast einen ganzen Film Schauen'
         ];
 
 function getRandomHobby() {
+    var d = new Date();
+    var monthDay = d.getMonth.concat("-", d.getDate);
+    // months are 0-indexed, dates are 1-indexed!
+    if (monthDay == "6-22"){
+        return "ihren Geburtstag Feiern"
+    } else if (monthDay = "6-5") {
+        return "ihren Geburtstag Planen"
+    }
+
     var n = Math.random(hobbies.length);
     var randomHobby = hobbies[(Math.random() * hobbies.length) | 0]
     return randomHobby;
